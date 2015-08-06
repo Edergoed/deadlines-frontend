7314479b88ab9214fa08f9e28f0f4d2a8d00e306
LiamEderzeel <liamederzeel@gmail.com>
2015-08-07 00:13:36 +0200

    Added doc to the deadlines view

--- app/app.states.js
+++ app/app.states.js
@@ -14,6 +14,18 @@ deadlinesApp.config([
     .config(["$locationProvider", function($locationProvider) {
       $locationProvider.html5Mode(true);
     }])
+
     .run(function ($rootScope){
         $rootScope.endPoint = 'http://localhost:8000'
     })
+
+    .run(function($rootScope) {
+        document.addEventListener("keyup", function(e) {
+            if (e.keyCode === 27)
+                $rootScope.$broadcast("escapePressed", e.target);
+        });
+
+        document.addEventListener("click", function(e) {
+            $rootScope.$broadcast("documentClicked", e.target);
+        });
+    });
---
+++ app/shares/nav/navController.js
@@ -0,0 +1,29 @@
+angular
+    .module('Deadlines').controller("NavCtrl", function($scope, $rootScope) {
+        $scope.leftVisible = false;
+        $scope.rightVisible = false;
+
+        $scope.close = function() {
+            $scope.leftVisible = false;
+            $scope.rightVisible = false;
+        };
+
+        $scope.showLeft = function(e) {
+            $scope.leftVisible = true;
+            e.stopPropagation();
+        };
+
+        $scope.showRight = function(e) {
+            $scope.rightVisible = true;
+            e.stopPropagation();
+        }
+
+        $rootScope.$on("documentClicked", _close);
+        $rootScope.$on("escapePressed", _close);
+
+        function _close() {
+            $scope.$apply(function() {
+                $scope.close(); 
+            });
+        }
+    });
