5e2953809d80e8c2b997f52a8f96ec7dd99f0309
LiamEderzeel <liamederzeel@gmail.com>
2015-08-08 13:58:32 +0200

    Working on menu

--- app/app.states.js
+++ app/app.states.js
@@ -19,13 +19,4 @@ deadlinesApp.config([
         $rootScope.endPoint = 'http://localhost:8000'
     })
 
-    .run(function($rootScope) {
-        document.addEventListener("keyup", function(e) {
-            if (e.keyCode === 27)
-                $rootScope.$broadcast("escapePressed", e.target);
-        });
-
-        document.addEventListener("click", function(e) {
-            $rootScope.$broadcast("documentClicked", e.target);
-        });
-    });
+    
