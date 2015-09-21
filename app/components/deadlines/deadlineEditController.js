d3b3743ed0e3fe6f31151cedfc5679a6a5d50a0a
LiamEderzeel <liamederzeel@gmail.com>
2015-09-21 23:58:50 +0200

    deadlines edit revector

--- app/components/deadlines/deadlineEditController.js
+++ app/components/deadlines/deadlineEditController.js
@@ -1,18 +1,14 @@
 app.controller('DeadlineEditCtrl', function($scope, $stateParams, deadline){
 
-    $scope.getDeadline = function(id){
-        deadline.getDeadline(id)
-        .then(function(res){
-            //$scope.deadlineChangeState('view');
-            $scope.selectedDeadline = deadline.deadline;
-            //succes
-        }, function(res){
-            //error
-        })
-    }
-
     $scope.updateDeadline = function(){
         if($scope.deadlineForm.$valid){
+            day = $scope.selectedDeadline.deadline.day
+            month = $scope.selectedDeadline.deadline.month
+            year = $scope.selectedDeadline.deadline.year
+            hour = $scope.selectedDeadline.deadline.time.split(':')[0];
+            minut = $scope.selectedDeadline.deadline.time.split(':')[1];
+
+            $scope.selectedDeadline.deadline.deadlineDateTime = year + '-' +month + '-' + day + ' ' + hour + ':' + minut + ':' + '00';
             deadline.updateDeadline($scope.userCurrent.id, $scope.selectedDeadline.deadline)
             .then(function(res){
                 //succes
@@ -25,6 +21,16 @@ app.controller('DeadlineEditCtrl', function($scope, $stateParams, deadline){
         }
     }
 
+    $scope.getDeadline = function(id){
+        deadline.getDeadline(id)
+        .then(function(res){
+            //$scope.deadlineChangeState('view');
+            $scope.selectedDeadline = deadline.deadline;
+            //succes
+        }, function(res){
+            //error
+        })
+    }
 
     if($stateParams.editID != null){
         $scope.getDeadline($stateParams.editID);
