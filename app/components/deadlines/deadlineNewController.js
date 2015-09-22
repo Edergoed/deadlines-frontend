d067448a427e7b08e01d791e4a30803fb1b771f1
LiamEderzeel <liamederzeel@gmail.com>
2015-09-22 03:02:35 +0200

    deadlines new now puts date on current date

--- app/components/deadlines/deadlineNewController.js
+++ app/components/deadlines/deadlineNewController.js
@@ -1,15 +1,22 @@
 app.controller('DeadlineNewCtrl', function($scope, deadline){
 
     $scope.init = function(){
+        $scope.dayCurrent = new Date().getDate();
+        console.log($scope.dayCurrent);
+        console.log($scope.monthCurrent);
         $scope.deadline = {};
         $scope.deadline.deadline = {};
         $scope.deadline.deadline.year = new Date().getFullYear();
-        $scope.deadline.deadline.month = new Date().getFullYear();
+        $scope.deadline.deadline.month = new Date().getMonth()+1;
+        $scope.deadline.deadline.day = new Date().getDate();
+        console.log($scope.deadline.deadline.month);
         $scope.$watch("deadline.deadline.month", function(newValue, oldValur){
             console.log("change");
             $scope.getDays($scope.deadline.deadline.year, $scope.deadline.deadline.month);
+            console.log($scope.deadline.deadline.year + ' ' + $scope.deadline.deadline.month);
         });
-        $scope.getDays($scope.deadline.deadline.year, $scope.deadline.deadline.month);
+            $scope.getMonths();
+            $scope.getYears();
     }
 
     $scope.getDays = function(year, month){
@@ -20,6 +27,33 @@ app.controller('DeadlineNewCtrl', function($scope, deadline){
             $scope.days[i] = i+1;
         }
         $scope.$digest;
+    }
+
+    $scope.getMonths = function(){
+
+        $scope.months = [
+            {'id': 1, 'name': 'January'},
+            {'id': 2, 'name': 'February'},
+            {'id': 3, 'name': 'March'},
+            {'id': 4, 'name': 'April'},
+            {'id': 5, 'name': 'May'},
+            {'id': 6, 'name': 'June'},
+            {'id': 7, 'name': 'July'},
+            {'id': 8, 'name': 'August'},
+            {'id': 9, 'name': 'September'},
+            {'id': 10, 'name': 'October'},
+            {'id': 11, 'name': 'November'},
+            {'id': 12, 'name': 'December'},
+        ];
+
+    }
+
+    $scope.getYears = function(){
+        $scope.years =[
+            new Date().getFullYear(),
+            new Date().getFullYear()+1,
+            new Date().getFullYear()+2
+        ]
     }
 
     $scope.createDeadline = function(){
