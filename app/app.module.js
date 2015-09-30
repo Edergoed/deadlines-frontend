196d09c8d39f88490c96498bce30d4d29799b8a2
LiamEderzeel <liamederzeel@gmail.com>
2015-09-30 19:44:05 +0200

    hot fix [deploy: live]

--- app/app.module.js
+++ app/app.module.js
@@ -6,8 +6,8 @@ var app = angular.module('Deadlines', [
 app
 .constant('urls', {
     BASE: 'http://deadlines.dev',
-    //BASE_API: 'http://188.226.192.169'
-   BASE_API: 'http://deadlinesapi.dev:88'
+    BASE_API: 'http://188.226.192.169'
+   //BASE_API: 'http://deadlinesapi.dev:88'
 });
 
 app.run(['$rootScope', '$location', function($rootScope, $location) {
